c6bc6b8a89ce488aebaf09de6e539a58ac3a54a5
Joydeep Roy <63199831+joy89-hue@users.noreply.github.com>
2020-12-08 21:32:47 +0530

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -28,30 +28,41 @@ scissors = '''
 ---.__(___)
 '''
 # Game setup
-player = int(input("What do you choose? Type 0 for rock, 1 for paper and 2 for scissors: "))
+"""
+3 SIMPLE RULES FOR RPS:
+Rock wins against scissors.
+Scissors win against paper.
+Paper wins against rock.
+"""
+game_image = [rock, paper, scissors]
+user_choice = int(input("What do you choose? Type 0 for rock, 1 for paper and 2 for scissors: "))
 
-if player == 0:
-  print(f"Player choose: {rock}")
-elif player == 1:
-  print(f"Player choose: {paper}")
+if user_choice in [0, 1, 2]:
+  print("You choose:\n")
+  print(game_image[user_choice])
+
+  computer_choice = randint(0, 2)
+  print("Computer choose:\n")
+  print(game_image[computer_choice])
+
+  if user_choice == computer_choice:
+    print("Its a tie")
+  else:
+    if user_choice == 0:
+      if computer_choice == 1:
+        print("You lose..")
+      else:
+        print("You win!")
+
+    elif user_choice == 1:
+      if computer_choice == 2:
+        print("You lose..")
+      else:
+        print("You win!")
+    elif user_choice == 2:
+      if computer_choice == 1:
+        print("You win!")
+      else:
+        print("You lose..")  
 else:
-  print(f"Player choose: {scissors}")
-
-num = [0, 1, 2]
-generated_choice = choice(num)
-computer = generated_choice
-
-if generated_choice == 0:
-  print(f"Computer choose: {rock}")
-
-elif generated_choice == 1:
-  print(f"Computer choose: {paper}")
-else:
-  print(f"Computer choose: {scissors}")  
-
-if player == computer:
-  print("Its a tie")
-elif player> computer:
-  print("You win!!")
-else:
-  print("You loose:(")
+  print("You typed invalid literal. You lose!")
